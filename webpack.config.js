6a535d575309a5c4b8280373c345a89376437c5f
Amirhossein Yaghoobi <a.yaghoobi.uni@gmail.com>
2018-09-06 11:16:05 +0430

    using webpack for building process

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+
+const iconsPattern = {
+	from: 'images/*',
+	dest: 'images'
+}
+const manifestJson = {
+	from: 'manifest.json',
+	to: 'manifest.json'
+}
+
+
+module.exports = (_, argv) => {
+	return {
+	    entry: './src/content.js',
+	    output: {
+		    filename: 'content.js',
+		    path: path.resolve(__dirname, 'dist')
+	    },
+	    optimization: {
+		    minimizer: [
+			    new UglifyJsPlugin({
+				    uglifyOptions: {
+					    compress: {
+						    drop_console: argv.mode === 'production'
+					    }
+				    }
+			    })
+		    ]
+	    },
+	    plugins: [
+		    new CleanWebpackPlugin(['dist']),
+		    new CopyWebpackPlugin([ iconsPattern, manifestJson ]),
+	    ],
+	    module: {
+		    rules: [
+			    {
+				    test: /\.js$/,
+				    exclude: /node_modules/,
+				    use: {
+					    loader: 'babel-loader',
+					    options: {
+						    presets: ['@babel/preset-env']
+					    }
+				    }
+			    },
+			    {
+				    test: /(\.jsx|\.js)$/,
+				    loader: 'eslint-loader',
+				    exclude: /node_modules/
+			    }
+		    ]
+	    }
+	}
+}
